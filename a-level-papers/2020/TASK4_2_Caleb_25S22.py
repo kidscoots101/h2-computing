7f95761e4e1deeacc5e8f48f3062a79d1cb4d906
Caleb Han <102847271+kidscoots101@users.noreply.github.com>
2026-07-27 16:37:03 +0800

    completed Person class

--- a-level-papers/2020/TASK4_2_Caleb_25S22.py
+++ a-level-papers/2020/TASK4_2_Caleb_25S22.py
@@ -7,8 +7,13 @@ class Person:
     def is_adult(self):
         return 2026 - int(self.date_of_birth[0:4]) >= 18
     def screen_name(self):
-        pass
+        full = ""
+        for char in self.full_name:
+            if char.isalpha() == True:
+                full += char
+        date = self.date_of_birth.split("-")
+        return full + date[1] + date[2]        
 
-
-person = Person("Caleb", "2000-10-10")
+person = Person("Caleb ,Han", "2000-10-10")
 print(person.is_adult())
+print(person.screen_name())
