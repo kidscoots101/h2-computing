3fd81e846a72bd24cfed71481f6341bf7fbe72c6
Caleb Han <102847271+kidscoots101@users.noreply.github.com>
2026-07-27 17:16:14 +0800

    finished task 4.2

--- a-level-papers/2020/TASK4_2_Caleb_25S22.py
+++ a-level-papers/2020/TASK4_2_Caleb_25S22.py
@@ -1,6 +1,8 @@
 
 # Task 4.2
 
+import sqlite3
+
 class Person:
     def __init__(self, name, birth):
         self.full_name = name
@@ -16,10 +18,12 @@ class Person:
         date = self.date_of_birth.split("-")
         return full + date[1] + date[2]        
 
+'''
 person = Person("Caleb Han", "2000-10-10")
 print(person.is_adult())
 print(person.screen_name())
 print("")
+'''
 
 # for staff
 class Staff(Person):
@@ -29,11 +33,13 @@ class Staff(Person):
         return True
     def screen_name(self):
         return super().screen_name() + "Staff"
-
+    
+'''
 staff = Staff("Aloysius Lee", "2000-05-19")
 print(staff.screen_name())
 print(staff.is_adult())
 print("")
+'''
 
 # for students
 
@@ -42,8 +48,51 @@ class Student(Person):
         super().__init__(name, birth)
     def is_adult(self):
         return False
-
+'''
 student = Student("Nolan Tan", "2019-09-22")
 print(student.screen_name())
 print(student.is_adult())
 print("")
+'''
+
+with open('people.txt', 'r') as file:
+    file = file.read().strip().split("\n") # 'John Tan,2000-06-01,Person'
+    connection = sqlite3.connect("school.db")
+    for obj in file:
+        obj = obj.split(",")
+        name = obj[0]
+        DOB = obj[1]
+        class_t = obj[2]
+        if class_t == "Person":
+            person = Person(name, DOB)
+            screen_name = person.screen_name()
+            is_adult = person.is_adult()
+            connection.execute(
+                f'''
+                INSERT INTO People(FullName, DateOfBirth, ScreenName, IsAdult) VALUES(?, ?, ?, ?)
+                ''',
+                (name, DOB, screen_name, is_adult) # 
+            )
+        elif class_t == "Student":
+            student = Student(name, DOB)
+            screen_name = student.screen_name()
+            is_adult = student.is_adult()
+            connection.execute(
+                f'''
+                INSERT INTO People(FullName, DateOfBirth, ScreenName, IsAdult) VALUES(?, ?, ?, ?)
+                ''',
+                (name, DOB, screen_name, is_adult)
+            )           
+
+        elif class_t == "Staff":
+            staff = Staff(name, DOB)
+            screen_name = staff.screen_name()
+            is_adult = staff.is_adult()
+            connection.execute(
+                f'''
+                INSERT INTO People(FullName, DateOfBirth, ScreenName, IsAdult) VALUES(?, ?, ?, ?)
+                ''',
+                (name, DOB, screen_name, is_adult)
+            )
+    connection.commit()
+    connection.close()
